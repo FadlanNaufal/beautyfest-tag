fdd0d04db80b6a19f2dd083953f7e3a35b0b6828
Git fadlan <git-fadlan@lalampa.me>
2022-10-10 08:16:33 +0700

    test

---
+++ index.js
@@ -0,0 +1,79 @@
+<script>
+      window.googletag = window.googletag || { cmd: [] };
+      googletag.cmd.push(function () {
+        googletag
+          .defineSlot(
+            "/253109699/Campaign/BFA_Homepage",
+            [[1, 1]],
+            "div-gpt-ad-slide-home1"
+          )
+          .setTargeting("pos", ["slide1"])
+          .addService(googletag.pubads());
+
+        googletag
+          .defineSlot(
+            "/253109699/Campaign/BFA_Homepage",
+            [[1, 1]],
+            "div-gpt-ad-slide-home2"
+          )
+          .setTargeting("pos", ["slide2"])
+          .addService(googletag.pubads());
+
+        googletag
+          .defineSlot(
+            "/253109699/Campaign/BFA_Homepage",
+            [[1, 1]],
+            "div-gpt-ad-slide-home3"
+          )
+          .setTargeting("pos", ["slide3"])
+          .addService(googletag.pubads());
+
+        googletag
+          .defineSlot(
+            "/253109699/Campaign/BFA_Homepage",
+            [[1, 1]],
+            "div-gpt-ad-slide-home4"
+          )
+          .setTargeting("pos", ["slide4"])
+          .addService(googletag.pubads());
+
+        googletag
+          .defineSlot(
+            "/253109699/Campaign/BFA_Homepage",
+            [[1, 1]],
+            "div-gpt-ad-slide-home5"
+          )
+          .setTargeting("pos", ["slide5"])
+          .addService(googletag.pubads());
+
+        googletag
+          .defineSlot(
+            "/253109699/Campaign/BFA_Homepage",
+            [[1, 1]],
+            "div-gpt-ad-slide-home6"
+          )
+          .setTargeting("pos", ["slide6"])
+          .addService(googletag.pubads());
+
+        googletag
+          .defineSlot(
+            "/253109699/Campaign/BFA_Homepage",
+            [[1, 1]],
+            "div-gpt-ad-slide-home7"
+          )
+          .setTargeting("pos", ["slide7"])
+          .addService(googletag.pubads());
+
+        googletag
+          .defineSlot(
+            "/253109699/Campaign/BFA_Homepage",
+            [[1, 1]],
+            "div-gpt-ad-slide-home8"
+          )
+          .setTargeting("pos", ["slide8"])
+          .addService(googletag.pubads());
+
+        googletag.pubads().enableSingleRequest();
+        googletag.enableServices();
+      });
+    </script>
